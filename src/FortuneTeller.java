d0d31a02dd2fb718e3eea8c99a793b75a36c842c
David Langford <langfod@gmail.com>
2018-01-19 10:20:55 -0500

    added and verified First name input and output

--- src/FortuneTeller.java
+++ src/FortuneTeller.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 /*
  * FortuneTeller:
  * a console application that will tell the user’s fortune based on data received from the user.
@@ -10,9 +12,16 @@
 public class FortuneTeller {
 
 	public static void main(String[] args) {
-
-		System.out.println("*[First Name]* *[Last Name]* will retire in *[# of years]* with *[bank balance]* in the bank,\r\n" + 
+		Scanner input = new Scanner(System.in);
+		
+		System.out.print("Please enter your First name: ");
+		String userFirstName = input.nextLine();
+		
+		System.out.println();
+		System.out.println(userFirstName + " *[Last Name]* will retire in *[# of years]* with *[bank balance]* in the bank,\r\n" + 
 				"a vacation home in *[location]*, and travel by *[mode of transporation]*.");	
+	
+	input.close();
 	}
 
 }
