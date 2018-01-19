d85913c02656892d54ad5978c5f2f08a697b29ad
David Langford <langfod@gmail.com>
2018-01-19 10:15:50 -0500

    console template output added

--- src/FortuneTeller.java
+++ src/FortuneTeller.java
@@ -11,10 +11,8 @@ public class FortuneTeller {
 
 	public static void main(String[] args) {
 
-		
 		System.out.println("*[First Name]* *[Last Name]* will retire in *[# of years]* with *[bank balance]* in the bank,\r\n" + 
-				"a vacation home in *[location]*, and travel by *[mode of transporation]*.");
-		
+				"a vacation home in *[location]*, and travel by *[mode of transporation]*.");	
 	}
 
 }
