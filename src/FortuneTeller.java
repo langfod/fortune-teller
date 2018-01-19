e6c8460dd06fb7cc2c9388841f2b9276f44eb479
David Langford <langfod@gmail.com>
2018-01-19 11:26:10 -0500

    added and verified user color input and help loop- NOTE: userColor will need to be removed from output in Part2!

--- src/FortuneTeller.java
+++ src/FortuneTeller.java
@@ -13,24 +13,47 @@ public class FortuneTeller {
 
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
-		
+
 		System.out.print("Please enter your First name: ");
 		String userFirstName = input.nextLine();
-		
+
 		System.out.print("Please enter your Last name: ");
 		String userLastName = input.nextLine();
-		
+
 		System.out.print("Please enter your age (as an integer): ");
 		int userAge = input.nextInt();
-		
+
 		System.out.print("Please enter your month of birth (as an integer 1-12): ");
 		int userBirthMonth = input.nextInt();
-		
+
+		String userColor;
+		Boolean userColorRecieved = false;
+		do {
+			System.out.println("Please enter you favorite ROYGBIV color:");
+			System.out.println("(If you do not know your ROYGBIV color names type HELP)");
+			userColor = input.next().trim().toLowerCase();
+			switch (userColor) {
+			case ("red"):
+			case ("orange"):
+			case ("yellow"):
+			case ("green"):
+			case ("blue"):
+			case ("indigo"):
+			case ("violet"):
+				userColorRecieved = true;
+				break;
+			default:
+				System.out.println("ROYGBIV colors are: Red, Orange, Yellow, Green, Blue, Indigo, Violet.\n");
+				break;
+			}
+		} while (!userColorRecieved);
+
 		System.out.println();
-		System.out.println(userFirstName + " " + userLastName + " will retire in " + userAge + "years with " + userBirthMonth + " in the bank,\r\n" + 
-				"a vacation home in *[location]*, and travel by *[mode of transporation]*.");	
-	
-	input.close();
+		System.out.println(
+				userFirstName + " " + userLastName + " will retire in " + userAge + "years with " + userBirthMonth
+						+ " in the bank,\r\n" + "a vacation home in *[location]*, and travel by " + userColor + ".");
+
+		input.close();
 	}
 
 }
