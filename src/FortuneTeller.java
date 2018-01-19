e35d2bd87540d90c189b5950f31f893a7c5f3ef6
David Langford <langfod@gmail.com>
2018-01-19 10:14:23 -0500

    add top level comment

--- src/FortuneTeller.java
+++ src/FortuneTeller.java
@@ -1,9 +1,20 @@
+/*
+ * FortuneTeller:
+ * a console application that will tell the user’s fortune based on data received from the user.
+ * 
+ * Author: David Langford
+ * Date  : Jan 19, 2018
+ * 
+ */
 
 public class FortuneTeller {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		
+		System.out.println("*[First Name]* *[Last Name]* will retire in *[# of years]* with *[bank balance]* in the bank,\r\n" + 
+				"a vacation home in *[location]*, and travel by *[mode of transporation]*.");
+		
 	}
 
 }
